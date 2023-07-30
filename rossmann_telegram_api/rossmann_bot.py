6ef833e590cc28f2c26ca147f75e3b267ff3d7f7
Soturno95 <edu.yori@gmail.com>
2023-07-30 19:02:37 -0300

    d1 print

--- rossmann_telegram_api/rossmann_bot.py
+++ rossmann_telegram_api/rossmann_bot.py
@@ -82,7 +82,7 @@ def predict(data):
 	r=requests.post(url,data=data,headers=header)
 	
 	print('Status Code {}'.format(r.status_code))
-	d1='a'
+	d1=pd.DataFrame(r.json(),columns=r.json()[0].keys())
 	try:
 		d1=pd.DataFrame(r.json(),columns=r.json()[0].keys())  # d1=df com coluna de predição
 	
